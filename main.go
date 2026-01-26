8b5f41c542ec7ac662c5b1f4c96dbee013e37997
AwEi <chenwei791129@livemail.tw>
2026-01-26 23:09:07 +0800

    fix: hide native window title to prevent text overlap

--- main.go
+++ main.go
@@ -42,7 +42,7 @@ func main() {
 		Mac: &mac.Options{
 			TitleBar: &mac.TitleBar{
 				TitlebarAppearsTransparent: true,
-				HideTitle:                  false,
+				HideTitle:                  true,
 				HideTitleBar:               false,
 				FullSizeContent:            true,
 				UseToolbar:                 false,
